85f88c14be27f376f5d858566f0cc6be757ea82b
StareStarrySky <zcbtyz@qq.com>
2024-01-02 13:33:33 +0800

    setTaskScheduler for websocket heartbeat

--- forex-server/forex-service/src/main/kotlin/xyz/starestarrysky/forex/server/config/WebSocketConfig.kt
+++ forex-server/forex-service/src/main/kotlin/xyz/starestarrysky/forex/server/config/WebSocketConfig.kt
@@ -3,7 +3,10 @@ package xyz.starestarrysky.forex.server.config
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Lazy
 import org.springframework.messaging.simp.config.MessageBrokerRegistry
+import org.springframework.scheduling.TaskScheduler
+import org.springframework.web.socket.config.WebSocketMessageBrokerStats
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer
 import xyz.starestarrysky.forex.server.property.StompConfig
@@ -16,14 +19,25 @@ class WebSocketConfig : WebSocketMessageBrokerConfigurer {
     @Autowired
     private lateinit var stompConfig: StompConfig
 
+    @Lazy
+    @Autowired
+    private lateinit var webSocketStats: WebSocketMessageBrokerStats
+
+    @Lazy
+    @Autowired
+    private lateinit var messageBrokerTaskScheduler: TaskScheduler
+
     override fun registerStompEndpoints(registry: StompEndpointRegistry) {
         stompConfig.endpoints.forEach {
             registry.addEndpoint(it.point).setAllowedOrigins(it.allowedOrigins)
         }
+        webSocketStats.loggingPeriod = stompConfig.statsLoggingPeriod
     }
 
     override fun configureMessageBroker(registry: MessageBrokerRegistry) {
         registry.enableSimpleBroker(stompConfig.broker)
+            .setTaskScheduler(messageBrokerTaskScheduler)
+//            .setHeartbeatValue(longArrayOf(10000, 10000))
         registry.setApplicationDestinationPrefixes(contextPath)
     }
 }
--- forex-server/forex-service/src/main/kotlin/xyz/starestarrysky/forex/server/property/StompConfig.kt
+++ forex-server/forex-service/src/main/kotlin/xyz/starestarrysky/forex/server/property/StompConfig.kt
@@ -3,19 +3,21 @@ package xyz.starestarrysky.forex.server.property
 import org.springframework.boot.context.properties.ConfigurationProperties
 import java.io.Serializable
 
-@ConfigurationProperties("push")
+@ConfigurationProperties("push.stomp")
 class StompConfig : Serializable {
     companion object {
-        private const val serialVersionUID = 591606041472408228L
+        private const val serialVersionUID = 2298227968805436544L
     }
 
     lateinit var broker: String
 
     lateinit var endpoints: List<StompEndpoint>
 
+    var statsLoggingPeriod: Long = 30 * 60 * 1000
+
     class StompEndpoint : Serializable {
         companion object {
-            private const val serialVersionUID = -4199954564360775922L
+            private const val serialVersionUID = 248074414613139692L
         }
 
         lateinit var point: String
